88c72fb001d6cc78f3c4a6a141c0b54c59995557
Leonid Zulin <leo.zulin@gmail.com>
2021-06-21 07:55:10 +0300

    Первая версия задачи 'Калькулятор'

---
+++ src/main/java/Ints.java
@@ -0,0 +1,22 @@
+// Interface для для работы с калькулятором
+public interface Ints {
+
+    //метод сложения
+    public int sum(int arg1, int arg2);
+
+    //метод вычитание
+    public int sub(int arg1, int arg2);
+
+    //метод умножение
+    public int mult(int arg1, int arg2);
+
+    // метод деление
+    public double div(int arg1, int arg2);
+
+    // метод возведения в степень
+    public int pow(int arg1, int arg2);
+
+    // метод вычисления квадратного корня
+    public double sqrt(int arg1);
+
+}
